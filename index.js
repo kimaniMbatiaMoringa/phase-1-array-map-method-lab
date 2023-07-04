03d0bf0311971424e785987bda0204e1fd694fce
kimaniMbatiaMoringa <kimani.mbatia@student.moringaschool.com>
2023-07-04 20:00:14 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -12,5 +12,63 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+ let tutorials2 = tutorials.map(function(arrItem){
+    let sentenceBrokenIntoWordsArray = arrItem.split(" ")                                       //Splits each sentence elements into individual words
+
+    let wordsStartWithUpperCase = sentenceBrokenIntoWordsArray.map(function(word){              //splits subarray into individual words                                                            
+     let word2 = word.charAt(0).toUpperCase() + word.substring(1, word.length)                     //Makes That word start with an uppercase letter, then adds the rest of the letters to the word
+      //console.log(word2)
+      return word2
+    })
+    console.log(wordsStartWithUpperCase.join(' '))
+    let upperCasedSentence =wordsStartWithUpperCase.join(' ')
+    return upperCasedSentence
+  })
+
+return tutorials2
+  
+
+
+
+/*   console.log(tutorials2)
+  return tutorials */
 }
+
+/* function firstLetterToUpperCase(arrayItem){
+  return arrayItem + " BASED"
+}
+
+tutorials2 = tutorials.map(function(arrItem){
+   return arrItem.charAt(0).toUpperCase() + arrItem.substring(1)
+})
+
+console.log(tutorials2)
+ */
+console.log(titleCased())
+/* 
+let sentenceBrokenIntoWordsArray = titleCased()
+ console.log(sentenceBrokenIntoWordsArray)
+
+
+  let upperCasedWords = sentenceBrokenIntoWordsArray.forEach(function(word){                     //For Every object in array
+
+    let wordsStartWithUpperCase = word.map(function(word){                                       //splits subarray into individual words                                                            
+      word2 = word.charAt(0).toUpperCase() + word.substring(1, word.length)                     //Makes That word start with an uppercase letter, then adds the rest of the letters to the word
+      //console.log(word2)
+      return word2
+    })
+
+    console.log(wordsStartWithUpperCase.join(' '))
+    let upperCasedSentence =wordsStartWithUpperCase.join(' ')
+
+    tutorials.push(upperCasedSentence)
+
+    return upperCasedSentence
+  })
+
+//console.log(tutorials)
+console.log(tutorials)
+ */
+
+//get the first word in that array map it to upper case
+//
